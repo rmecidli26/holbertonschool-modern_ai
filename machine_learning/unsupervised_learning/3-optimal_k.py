70ff94c977cc4852ee5c6edd8f4e20e200f5cb6e
rmecidli26 <rmecidli26@gmail.com>
2026-06-12 16:34:28 +0000

    2-exercises

--- machine_learning/unsupervised_learning/3-optimal_k.py
+++ machine_learning/unsupervised_learning/3-optimal_k.py
@@ -6,18 +6,15 @@ K_Means = __import__('2-k_means').K_Means
 
 def optimal_k(X, max_clusters, random_state):
     """Evaluates K-Means clustering quality using inertia and silhouette score"""
-    ks = list(range(1, max_clusters + 1))
+    ks = list(range(2, max_clusters + 1))
     inertia_values = []
     silhouette_values = []
 
     for k in ks:
         model = K_Means(X, n_clusters=k, random_state=random_state)
         inertia_values.append(model.inertia_)
-
-        if k == 1:
-            silhouette_values.append(0.0)
-        else:
-            score = metrics.silhouette_score(X, model.labels_)
-            silhouette_values.append(score)
+        
+        score = metrics.silhouette_score(X, model.labels_)
+        silhouette_values.append(score)
 
     return ks, inertia_values, silhouette_values
